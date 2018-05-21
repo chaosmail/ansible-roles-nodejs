29804f77921488e7d8bf96c1f9190838214a8416
chaosmail <office@chaosmail.at>
2018-05-21 22:38:55 +0200

    add docker images to travis (#2)
    * add docker images to travis
    
    * fix ubuntu and centos
    
    * fix errors
    
    * fix repo name
    
    * fix ubuntu issues
    
    * fix ubuntu issues
    
    * fix ubuntu 14
    
    * upgrade setup tools
    
    * check if node and npm are installed
    
    * fix python comp tools
    
    * fix ubuntu 14 sni error

---
+++ tests/run_test.sh
@@ -0,0 +1,24 @@
+# Install a specific Ansible version
+if [ -n "$ANSIBLE" ]; then
+
+  # Update Python and Pip
+  apt-get update && apt-get -y install python python-pip ||
+    yum install -y python python-pip;
+
+  # Install specific Ansible version
+  pip install -Iv ansible==$ANSIBLE;
+
+fi
+
+# Debug output
+pwd
+cat /etc/*-release;
+ansible --version; 
+
+# Run the test.yml playbook
+cd /root/$ROLE;
+ansible-playbook tests/test.yml -i tests/inventory;
+
+# Check if NodeJS and NPM are installed
+node --version;
+npm --version;
